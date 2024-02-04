93492fc586172da45f0eb61afbf0dd7b389d3da1
Ghifani <ghifaniikhsan114@gmail.com>
2024-02-04 17:41:45 +0700

    Membuat header dan landing page

---
+++ features/header/header.php
@@ -0,0 +1,11 @@
+<header>
+    <div class="container">
+        <div class="logo">
+            <h1 class="text-logo">Steak</h1>
+            <img src="public/assets/logo-meat.png" alt="Logo" class="img-logo" />
+        </div>
+        <nav>
+            <button>Login</button>
+        </nav>
+    </div>
+</header>
---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>GhipSteak</title>
+
+    <!-- Link Font -->
+    <link href="https://fonts.googleapis.com/css2?family=Allerta+Stencil&family=Noto+Sans+Mono:wght@200;300;400&family=Orbitron:wght@400;500;600;700;800;900&family=Pixelify+Sans:wght@400;500;600;700&family=Roboto:wght@400;500;700;900&family=Rubik+Bubbles&family=Sixtyfour&display=swap" 
+    rel="stylesheet">
+
+    <!-- Link CSS -->
+    <link rel="stylesheet" href="styles/header.css">
+    <link rel="stylesheet" href="styles/style.css">
+
+</head>
+
+
+<style>
+    body {
+        background-image: url("public/assets/bg-resto.jpg");
+        background-size: cover;
+    }
+</style>
+
+<body>
+    <?php include("features/header/header.php") ?>
+
+    <section>
+        <main>
+            <h1>This Is My Resto</h1>
+        </main>
+    </section>
+</body>
+</html>
