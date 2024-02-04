9d27a2dbdb3ed80aaee3197b53a64e5815d67308
Ghifani <ghifaniikhsan114@gmail.com>
2024-02-04 23:24:40 +0700

    Membuat Page Login CSS dan HTML

--- features/login/login.php
+++ features/login/login.php
@@ -1,15 +1,36 @@
-<?php
-    echo "This is page Login";
-?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>GhipSteak Login</title>
+    <!-- Link Font -->
+    <link href="https://fonts.googleapis.com/css2?family=Allerta+Stencil&family=Noto+Sans+Mono:wght@200;300;400&family=Orbitron:wght@400;500;600;700;800;900&family=Pixelify+Sans:wght@400;500;600;700&family=Roboto:wght@400;500;700;900&family=Rubik+Bubbles&family=Sixtyfour&display=swap" 
+    rel="stylesheet">
+
+    <link rel="stylesheet" href="style.css">
 </head>
+
 <body>
-    
+
+    <section>
+        <main>
+            <div class="container-text">
+                <h1>Steak</h1>
+                <p>Jika Anda seoarang admin maka masukkan username dan password anda untuk masuk ke akun Steak.</p>
+            </div>
+
+
+            <div class="container-form">
+                <form action="">
+                    <input type="text" placeholder="Username">
+                    <input type="password" placeholder="Password">
+                    <button>Login</button>
+                    <hr>
+                </form>
+
+            </div>
+        </main>
+    </section>
 </body>
 </html>
